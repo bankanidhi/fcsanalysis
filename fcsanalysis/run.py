3635c5a8457da589d23dfc6302345e43d91b80a9
Kesavan PS <pskesavan@tifrh.res.in>
2018-07-13 15:34:55 +0530

    pretty print the filenames for --list

--- fcsanalysis/run.py
+++ fcsanalysis/run.py
@@ -7,7 +7,12 @@ def run():
     file_list = find_files(options.key)
 
     if options.list:
-        print(file_list)
+        for f in file_list:
+            print(f)
+        print("======================")
+        print("Total files found:", len(files))
+        print("======================")
+        exit()
     else:
         for file in file_list:
             analyse_data(file, numcol=5)
