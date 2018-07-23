e021157fb4ecc500d3e6cd62d2a0644e0d65dda6
Kesavan PS <pskesavan@tifrh.res.in>
2018-07-23 15:46:50 +0530

    some comments

--- fcsanalysis/run.py
+++ fcsanalysis/run.py
@@ -3,9 +3,12 @@ from .args import arguments
 
 
 def run():
+    # Stores the parsed arguments
     options = arguments()
+    # Get a list of files that match the keyword as given with the -k arg
     file_list = find_files(options.k)
 
+    # If the user wants to perform a dry run.
     if options.list:
         print("----------------------")
         for f in file_list:
@@ -14,6 +17,8 @@ def run():
         print("Total files found:", len(file_list))
         print("----------------------")
         exit()
+
+    # The actually analysis happens after this.
     else:
         for file in file_list:
             analyse_data(file, int(options.numcol))
