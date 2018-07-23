465ae3304f5fc9d9dffafa619fcdab5ae68cc0ad
Kesavan PS <pskesavan@tifrh.res.in>
2018-07-23 15:38:04 +0530

    working code -- changed incorrect variable names, imports

--- fcsanalysis/run.py
+++ fcsanalysis/run.py
@@ -1,17 +1,18 @@
-from .correlate import analyse_data
+from .correlate import analyse_data, find_files
 from .args import arguments
 
 
 def run():
     options = arguments()
-    file_list = find_files(options.key)
+    file_list = find_files(options.k)
 
     if options.list:
+        print("----------------------")
         for f in file_list:
             print(f)
-        print("======================")
-        print("Total files found:", len(files))
-        print("======================")
+        print("----------------------")
+        print("Total files found:", len(file_list))
+        print("----------------------")
         exit()
     else:
         for file in file_list:
